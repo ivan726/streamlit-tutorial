75f68ce7bdd9fb4a6b15caf2a6fc05f9b9f84994
idop726 <idop726@gmail.com>
2024-07-09 22:18:03 +0000

    New questions

--- tutor/preguntas.py
+++ tutor/preguntas.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-v0_8-whitegrid")
 
 st.title(":question: Algunas :red[preguntas]")
 
@@ -24,4 +26,53 @@ with st.expander("¿Cómo se podría ingresar un rango en Streamlit?"):
         ini = int(ini)
         fin = int(fin)
         st.write(f"Rango ingresado: $[{ini}, {fin}]$")
+
+with st.expander("¿Cómo puedo graficar valores tabulados?"):
+    with st.echo():
+        c1, c2 = st.columns([1, 3])
+        with c1:
+            num = int(st.number_input("Número de datos a ingresar:", value=3))
+            datos = np.zeros((num, 2))
+            datos[:, 0] = np.arange(num)
+            datos[:, 1] = np.random.randint(-10, 10, num)
+            datos = st.data_editor(datos, use_container_width=True, column_config={"0":st.column_config.NumberColumn("x"), "1":st.column_config.NumberColumn("y")})
+
+        with c2:
+            fig, ax = plt.subplots()
+            ax.plot(datos[:,0], datos[:,1])
+            ax.axvline(0, color="gray")
+            ax.axhline(0, color="gray")
+            st.pyplot(fig)
+
+with st.expander("¿Cómo puedo tabular una función?"):
+    with st.echo():
+        c1, c2 = st.columns([1,3])
+        with c1:
+            st.markdown("""
+            **Función a graficar:** 
+            $$
+            f(x) = x^2
+            $$""")
+            num = int(st.number_input("Puntos:", value=3))
+
+            c11, c12 = st.columns(2)
+            with c11:
+                datos_x = np.linspace(-5, 5, num)
+                datos_x = st.data_editor(datos_x, use_container_width=True, column_config={"value":st.column_config.NumberColumn("x")})
+            with c12:
+                datos_y = datos_x**2
+                datos_y = st.data_editor(datos_y, use_container_width=True, column_config={"value":st.column_config.NumberColumn("f(x)", disabled=True)})
+
+        with c2:
+            # creando vectores de la curva
+            vec_x = np.linspace(min(datos_x), max(datos_x), 100)
+            vec_y = vec_x**2
+
+            fig, ax = plt.subplots()        # grafico
+            ax.axvline(0, color="gray")     # eje x
+            ax.axhline(0, color="gray")     # eje y
+            ax.plot(vec_x, vec_y)           # linea
+            ax.scatter(datos_x, datos_y, zorder=2)      # puntos
+            st.pyplot(fig)                  # mostrar
+
 

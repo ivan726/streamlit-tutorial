72bc539c1c52c8bca1c3967196f4fb1227c2ea2d
idop726 <idop726@gmail.com>
2024-07-12 19:00:20 +0000

    New proyects

--- tutor/expos.py
+++ tutor/expos.py
@@ -10,3 +10,11 @@ pe1["Código"] = pe1["Código"].astype("string")
 pe1 = pe1.sample(frac=1, random_state=1107)
 
 st.dataframe(pe1, hide_index=True, use_container_width=True, column_config={"Código":st.column_config.TextColumn(), "Link":st.column_config.LinkColumn()})
+
+st.header("Grupo :orange[PE2]", divider="orange")
+
+pe2 = pd.read_csv("static/pe2.csv")
+pe2["Código"] = pe2["Código"].astype("string")
+pe2 = pe2.sample(frac=1, random_state=1107)
+
+st.dataframe(pe2, hide_index=True, use_container_width=True, column_config={"Código":st.column_config.TextColumn(), "Link":st.column_config.LinkColumn()})
